8650b59742da9bd0d8c84a9583f7bd359f1128e0
cookie <cookiemakefun@qq.com>
2023-05-15 22:29:41 +0800

    猜数游戏

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,36 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Fail to read line");
+    let mut rng = rand::thread_rng();
+    let secret_number = rng.gen_range(1..=100);
 
-    println!("You guess: {guess}")
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Fail to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guess: {guess}");
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
