49359c4b6e97f3e2056b761c37f3923f4b29aa53
cookie <cookiemakefun@qq.com>
2023-05-24 22:23:52 +0800

    结构体的方法

---
+++ method_syntax/target/debug/deps/method_syntax-84cc3fe8aa06c00b.d
@@ -0,0 +1,5 @@
+/home/cookie/project/method_syntax/target/debug/deps/method_syntax-84cc3fe8aa06c00b: src/main.rs
+
+/home/cookie/project/method_syntax/target/debug/deps/method_syntax-84cc3fe8aa06c00b.d: src/main.rs
+
+src/main.rs:
---
+++ method_syntax/target/debug/method_syntax.d
@@ -0,0 +1,1 @@
+/home/cookie/project/method_syntax/target/debug/method_syntax: /home/cookie/project/method_syntax/src/main.rs
